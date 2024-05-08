0912c208522580c1e921a2fc1a5cc954fbe7a646
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2024-05-08 23:45:59 +0530

    Time: 34 ms (60.34%), Space: 16.6 MB (57.68%) - LeetHub

---
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        else:
+            res = n * 2
+            return res
+        
