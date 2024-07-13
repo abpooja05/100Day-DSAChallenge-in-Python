9a4233d4eafc525468b67965bb1ee8b23f8cecc3
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2024-07-13 23:16:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def armstrongNumber (self, n):
+        # code here 
+        str_n = str(n)
+        total = 0
+        for digit in str_n:
+            total += int(digit) ** 3
+        
+        
+        if total == n:
+            return "true"
+        else:
+            return "false"
+        
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = input()
+        n = int(n)
+        ob = Solution()
+        print(ob.armstrongNumber(n))
+
+# } Driver Code Ends
