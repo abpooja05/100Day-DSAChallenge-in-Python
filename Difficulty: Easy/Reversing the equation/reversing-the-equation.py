26f639fa064d751bfb108fb15fb8755c7312d6dd
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2024-08-30 23:34:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reversing the equation/reversing-the-equation.py
+++ Difficulty: Easy/Reversing the equation/reversing-the-equation.py
@@ -4,17 +4,41 @@ class Solution:
     def reverseEqn(self, s):
         # code here
         
+        # num = ""
+        # st = ""
+        # for i in range(len(s)-1, -1, -1):
+        #     if s[i].isdigit() == False:
+        #         st += num[::-1] + s[i]
+        #         num =""
+        #     else:
+        #         num += s[i]
+        # st += num[::-1]
+        # return st
+        
+        res_str = ""
         num = ""
-        st = ""
-        for i in range(len(s)-1, -1, -1):
-            if s[i].isdigit() == False:
-                st += num[::-1] + s[i]
-                num =""
+        for c in s[::-1]:
+            if c == '+' or c == '-' or c == '/' or c == '*':
+                # reverse the number (incase of more than one digit)
+                num = num[::-1]
+                res_str += num
+                num = ""
+                
+                # add in result
+                res_str += c
             else:
-                num += s[i]
-        st += num[::-1]
-        return st
+                num += c
+        
+        # reverse the number (incase of more than one digit)
+        num = num[::-1]
+        res_str += num
+        
+        return res_str
+        
+        
+
             
+        
 
 
 #{ 
