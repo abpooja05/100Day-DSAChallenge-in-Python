0ef016c703ec95fb5049fbd2b61d5ec8f267436f
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-07 23:29:40 +0530

    Time: 0 ms (100%), Space: 17.5 MB (99.73%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -1,15 +1,28 @@
 class Solution:
-    def check(self, nums: List[int]) -> bool:
-        n = len(nums)
-        # Find the pivot point where the rotation occurs
-        pivot = 0
-        for i in range(1, n):
-            if nums[i] < nums[i - 1]:
-                pivot = i
+    def check(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxNum, minNum, minIdx = max(nums), min(nums), 0
+        for i in range(1, len(nums)):
+            if [nums[i-1], nums[i]] == [maxNum, minNum]:
+                minIdx = i                
                 break
+        return nums[minIdx:] + nums[:minIdx] == sorted(nums)
+    
+    ###other method
+#     def check(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         # Find the pivot point where the rotation occurs
+#         pivot = 0
+#         for i in range(1, n):
+#             if nums[i] < nums[i - 1]:
+#                 pivot = i
+#                 break
         
-        # Check if the array is sorted in non-decreasing order after rotation
-        for i in range(1, n):
-            if nums[(pivot + i) % n] < nums[(pivot + i - 1) % n]:
-                return False
-        return True
+#         # Check if the array is sorted in non-decreasing order after rotation
+#         for i in range(1, n):
+#             if nums[(pivot + i) % n] < nums[(pivot + i - 1) % n]:
+#                 return False
+#         return True
