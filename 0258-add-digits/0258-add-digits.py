c5a648c47210ad64eb4dc5f29b7cdfb0e21ae17b
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-09 23:43:10 +0530

    Time: 4 ms (4.88%), Space: 17.9 MB (15.82%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -2,7 +2,7 @@ class Solution:
     def addDigits(self, num: int) -> int:
         total = 0
         for digit in str(num):
-          total += int(digit)
-          if total >= 10:
-            total = self.addDigits(total)
+            total += int(digit)
+            if total >= 10:
+                total = self.addDigits(total)
         return total
