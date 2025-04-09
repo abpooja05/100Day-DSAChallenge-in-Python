08768c950d148b4834bb1c0828f5c96d3b1ae4fd
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-09 23:20:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
@@ -1,27 +1,38 @@
-#User function Template for python3
-
-class Solution:
-    def lcmAndGcd(self, A, B):
-        
-        x = A * B   # A is divident and B is divisor
-        while B != 0:
-            A, B = B, A%B
-        return [x//A, A]
-            
-
-
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
+from typing import List
 import math
-if __name__ == '__main__': 
-    t = int (input ())
-    for _ in range (t):
-        A,B=map(int,input().split())
-        
-        ob = Solution()
-        ptr = ob.lcmAndGcd(A,B)
-        
-        print(ptr[0],ptr[1])
+
+
+# } Driver Code Ends
+
+
+#User function Template for python3
+
+class Solution:
+    def lcmAndGcd(self, a : int, b : int) -> List[int]:
+        # code here
+        mul = a*b
+        while b != 0:
+            a, b = b, a%b
+            res = [mul//a, a]
+        return res
+
+
+#{ 
+ # Driver Code Starts.
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        a = int(input())
+        b = int(input())
+        obj = Solution()
+        res = obj.lcmAndGcd(a, b)
+        print(f"{res[0]} {res[1]}")
+        print("~")
+
 # } Driver Code Ends
