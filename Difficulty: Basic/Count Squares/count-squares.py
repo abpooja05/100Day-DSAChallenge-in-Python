f143cb8e92a886502d68fc1e7b165aa5faf72750
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-09 23:22:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Squares/count-squares.py
+++ Difficulty: Basic/Count Squares/count-squares.py
@@ -1,21 +1,22 @@
 #User function Template for python3
 
 class Solution:
-    def countSquares(self, N):
+    def countSquares(self, n):
         # code here 
-        return int(math.sqrt(N-1))
-
+        return int(math.sqrt(n-1))
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 import math
 
-if __name__ == '__main__': 
-    t = int (input ())
-    for _ in range (t):
-        N=int(input())
-        
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+
         ob = Solution()
         print(ob.countSquares(N))
+        print("~")
+
 # } Driver Code Ends
