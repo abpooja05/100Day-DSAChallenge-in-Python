83f8ad68364ba20963c73778dac1995ad51e7832
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-10 23:23:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reversing the equation/reversing-the-equation.py
+++ Difficulty: Easy/Reversing the equation/reversing-the-equation.py
@@ -3,43 +3,25 @@
 class Solution:
     def reverseEqn(self, s):
         # code here
-        
-        # num = ""
-        # st = ""
-        # for i in range(len(s)-1, -1, -1):
-        #     if s[i].isdigit() == False:
-        #         st += num[::-1] + s[i]
-        #         num =""
-        #     else:
-        #         num += s[i]
-        # st += num[::-1]
-        # return st
-        
         res_str = ""
         num = ""
+        
         for c in s[::-1]:
-            if c == '+' or c == '-' or c == '/' or c == '*':
-                # reverse the number (incase of more than one digit)
-                num = num[::-1]
+            if c == "+" or c == "-" or c == "/" or c == "*":
+                num = num[::-1] #if "c" is a two or more digit number
                 res_str += num
                 num = ""
                 
-                # add in result
+                #add in the result
                 res_str += c
+                
             else:
                 num += c
-        
-        # reverse the number (incase of more than one digit)
+                
         num = num[::-1]
         res_str += num
-        
         return res_str
-        
-        
-
-            
-        
-
+                
 
 #{ 
  # Driver Code Starts
@@ -52,4 +34,5 @@ if __name__ == '__main__':
         ob = Solution()
         ans = ob.reverseEqn(s)
         print(ans)
+        print("~")
 # } Driver Code Ends
