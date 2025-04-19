8155cbcfc7adfcae6e2ce67d7ccbdb707bd539f9
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-19 22:04:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.py
+++ Difficulty: Medium/Merge Sort/merge-sort.py
@@ -1,70 +1,59 @@
-#User function Template for python3
-
-class Solution:
-    def merge(self, arr, l, m, r):
-        # Lengths of the two sub-arrays
-        len1 = m - l + 1
-        len2 = r - m
-        
-        # Create temporary arrays
-        first = arr[l:m+1]
-        second = arr[m+1:r+1]
-        
-        # Initialize indices
-        index1 = 0
-        index2 = 0
-        mainarrayindex = l
-        
-        # Merge the two arrays
-        while index1 < len1 and index2 < len2:
-            if first[index1] <= second[index2]:
-                arr[mainarrayindex] = first[index1]
-                index1 += 1
-            else:
-                arr[mainarrayindex] = second[index2]
-                index2 += 1
-            mainarrayindex += 1
-        
-        # Copy any remaining elements from the first array
-        while index1 < len1:
-            arr[mainarrayindex] = first[index1]
-            index1 += 1
-            mainarrayindex += 1
-        
-        # Copy any remaining elements from the second array
-        while index2 < len2:
-            arr[mainarrayindex] = second[index2]
-            index2 += 1
-            mainarrayindex += 1
-        
-    def mergeSort(self, arr, l, r):
-        if l < r:
-            # Find the middle point
-            m = (l + r) // 2
-            
-            # Sort first and second halves
-            self.mergeSort(arr, l, m)
-            self.mergeSort(arr, m + 1, r)
-            
-            # Merge the sorted halves
-            self.merge(arr, l, m, r)
-
-
-
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
+# } Driver Code Ends
 
-import sys
-input = sys.stdin.readline
+
+#User function Template for python3
+
+class Solution:
+    def mergeSort(self,arr, l, r):
+        def merge(arr,l,m,r):
+            temp=[]
+            start=l
+            end=m+1
+            while start<=m and end<=r:
+                if arr[start]<=arr[end]:
+                    temp.append(arr[start])
+                    start+=1
+                else:
+                    temp.append(arr[end])
+                    end+=1
+            while start<=m:
+                temp.append(arr[start])
+                start+=1
+            while end<=r:
+                temp.append(arr[end])
+                end+=1
+            for i in range(l,r+1):
+                arr[i]=temp[i-l]
+            return arr
+        if l>=r:
+            return
+        mid=(l+r)//2
+        self.mergeSort(arr,l,mid)
+        self.mergeSort(arr,mid+1,r)
+        merge(arr,l,mid,r)
+                
+                
+        
+
+
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
 if __name__ == "__main__":
-    t=int(input())
-    for i in range(t):
-        n=int(input())
-        arr=list(map(int,input().split()))
-        Solution().mergeSort(arr, 0, n-1)
-        for i in range(n):
-            print(arr[i],end=" ")
-        print()
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ob.mergeSort(arr,0,len(arr)-1)
+        print(*arr)
+        print("~")
+        t -= 1
+
+
 # } Driver Code Ends
