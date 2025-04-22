89fb139f74d7b0f2022e6ff700e61c021e39a735
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-22 22:42:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.py
+++ Difficulty: Medium/Minimum sum/minimum-sum.py
@@ -1,60 +1,69 @@
 #User function Template for python3
 
 class Solution:
-    def solve(self, arr, n):
+    def minSum(self, arr):
         # code here
-        if n==1:
-            return(str(arr[0]))
-        
         arr.sort()
-        
-        num1=""
-        num2=""
-        
-        for i in range(n):
-            if i%2==0:
-                num1+=str(arr[i])
+        i = 0
+        str1 = ""
+        str2 = ""
+        while i<len(arr):
+            if arr[i]==0:
+                i+=1
+                continue
+            if i&1==0:
+                str1+=str(arr[i])
             else:
-                num2+=str(arr[i])
-                
-        sum1=int(num1)+int(num2)
-        return(sum1)
+                str2+=str(arr[i])
+            i+=1
+        
+        m = len(str1)-1
+        n = len(str2)-1
+        carry = 0
+        res = ""
+        while m>=0 and n>=0:
+            total = int(str1[m])+int(str2[n])+carry
+            value = total%10
+            carry = total//10
+            res = str(value) + res
+            m-=1
+            n-=1
+        
+        while m>=0:
+            total = int(str1[m]) + carry
+            value = total%10
+            carry = total//10
+            res = str(value) + res
+            m-=1
+        
+        while n>=0:
+            total = int(str2[n]) + carry
+            value = total%10
+            carry = total//10
+            res = str(value) + res
+            n-=1
+        
+        if carry>0:
+            res = str(carry) + res
+        
+        return res
 
-# class Solution:
-#   def solve(self,arr, n):
- 
-#     # sort the array
-#     arr.sort()
- 
-#     # let two numbers be a and b
-#     a = 0
-#     b = 0
-#     for i in range(n):
- 
-#         # Fill a and b with every alternate
-#         # digit of input array
-#         if (i % 2 != 0):
-#             a = a * 10 + arr[i]
-#         else:
-#             b = b * 10 + arr[i]
- 
-#     # return the sum
-#     return a + b
+        
+
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-
-
 if __name__ == '__main__':
     tc = int(input())
     while tc > 0:
-        n = int(input())
         arr = list(map(int, input().strip().split()))
         ob = Solution()
-        ans = ob.solve(arr, n)
+        ans = ob.minSum(arr)
         print(ans)
         tc -= 1
+
+        print("~")
 
 # } Driver Code Ends
