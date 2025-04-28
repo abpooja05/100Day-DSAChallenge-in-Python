1aa60f7a6b3762695b0f49c8af7fdf6c7101d2fc
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-04-28 23:36:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.py
@@ -0,0 +1,82 @@
+
+class Solution:
+    #User function Template for python3
+    #Function to count inversions in the array.
+    def inversionCount(self, arr):
+        # Your Code Here
+        # Function to use modified merge sort to count inversions
+        def merge_sort(arr, temp, left, right):
+            inv_count = 0
+            if left < right:
+                mid = (left + right) // 2
+                inv_count += merge_sort(arr, temp, left, mid)
+                inv_count += merge_sort(arr, temp, mid + 1, right)
+                inv_count += merge(arr, temp, left, mid, right)
+            return inv_count
+        
+        def merge(arr, temp, left, mid, right):
+            inv_count = 0
+            i = left  # Starting index for left subarray
+            j = mid + 1  # Starting index for right subarray
+            k = left  # Starting index to be sorted
+            
+            while i <= mid and j <= right:
+                if arr[i] <= arr[j]:
+                    temp[k] = arr[i]
+                    i += 1
+                else:
+                    temp[k] = arr[j]
+                    inv_count += (mid - i + 1)
+                    j += 1
+                k += 1
+            
+            # Copy the remaining elements of left subarray
+            while i <= mid:
+                temp[k] = arr[i]
+                k += 1
+                i += 1
+            
+            # Copy the remaining elements of right subarray
+            while j <= right:
+                temp[k] = arr[j]
+                k += 1
+                j += 1
+            
+            # Copy the sorted subarray back to original array
+            for idx in range(left, right + 1):
+                arr[idx] = temp[idx]
+            
+            return inv_count
+        
+        n = len(arr)
+        temp = [0] * n
+        return merge_sort(arr, temp, 0, n - 1)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        a = list(map(int, input().strip().split()))
+        obj = Solution()
+        print(obj.inversionCount(a))
+        print("~")
+
+# } Driver Code Ends
