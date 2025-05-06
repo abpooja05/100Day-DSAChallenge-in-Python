5b7b38f596328bfe3816f6ba15e4d89f5927016c
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-05-06 22:14:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.py
@@ -0,0 +1,65 @@
+class Solution:
+    # Function to return a list of integers denoting spiral traversal of matrix.
+    def spirallyTraverse(self, mat):
+        # code here
+        if not mat:
+            return []
+        
+        rows = len(mat)
+        cols = len(mat[0])
+        top = 0
+        bottom = rows - 1
+        left = 0
+        right = cols - 1
+        result = []
+        
+        while top <= bottom and left <= right:
+            # Traverse from left to right on top row
+            for i in range(left, right + 1):
+                result.append(mat[top][i])
+            top += 1
+            
+            # Traverse from top to bottom on right column
+            for i in range(top, bottom + 1):
+                result.append(mat[i][right])
+            right -= 1
+            
+            if top <= bottom:
+                # Traverse from right to left on bottom row
+                for i in range(right, left - 1, -1):
+                    result.append(mat[bottom][i])
+                bottom -= 1
+            
+            if left <= right:
+                # Traverse from bottom to top on left column
+                for i in range(bottom, top - 1, -1):
+                    result.append(mat[i][left])
+                left += 1
+        
+        return result
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        r = int(data[index])
+        c = int(data[index + 1])
+        index += 2
+        matrix = []
+        for i in range(r):
+            row = list(map(int, data[index:index + c]))
+            matrix.append(row)
+            index += c
+
+        solution = Solution()
+        result = solution.spirallyTraverse(matrix)
+        print(" ".join(map(str, result)))
+        print("~")
+
+# } Driver Code Ends
