7d60f50e39ed7a03bc7bb6974a4fa46a9cfb5891
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-05-19 22:55:02 +0530

    Time: 146 ms (22.47%), Space: 32.5 MB (66.29%) - LeetHub

---
+++ 2670-make-k-subarray-sums-equal/2670-make-k-subarray-sums-equal.py
@@ -0,0 +1,20 @@
+class Solution:
+    def makeSubKSumEqual(self, arr: List[int], k: int) -> int:
+        from statistics import median
+
+        n = len(arr)
+        g = gcd(n, k)  # Number of groups
+
+        res = 0
+        for start in range(g):
+            group = []
+            i = start
+            while True:
+                group.append(arr[i])
+                i = (i + k) % n
+                if i == start:
+                    break
+            group.sort()
+            m = group[len(group) // 2]
+            res += sum(abs(x - m) for x in group)
+        return res
